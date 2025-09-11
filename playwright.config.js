4eea33afad563f8313c7924b29a90c0bc68edd45
Juan Bolsi <juan.m.bolsi@gmail.com>
2025-09-11 11:18:42 -0300

    Added screenshot on failure to config

--- playwright.config.js
+++ playwright.config.js
@@ -29,6 +29,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://localhost:8080',
 
+    screenshot: 'only-on-failure',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
